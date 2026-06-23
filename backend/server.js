82851e042836357c32115b81f7dbd7670fb227c6
Snow <meghabhardwaj1112@gmail.com>
2026-06-23 15:23:24 +0530

    Completed user authentication with login and register

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const connectDB=require ("./config/db");
+const authRoutes=require("./routes/authRoutes");
+require("dotenv").config();
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Blog API Running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.use("/api/auth",authRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
